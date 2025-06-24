15eef6f312c3b480e1f5b197e2a789616b354a10
Aman Kumar <ak3722032@gmail.com>
2025-06-24 11:18:58 +0530

    revert env file

--- db/sql_conn.js
+++ db/sql_conn.js
@@ -5,7 +5,7 @@ dotenv.config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  // password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
@@ -25,6 +25,5 @@ promisePool.query('SELECT 1')
 
 // properly export the pool for use elsewhere
 export default promisePool;
-
 
 
